1fc0aba7d05cf67c8a62ad8fd9f8c8d184868f31
Braulio Zapata <brauliozapatad@gmail.com>
2020-07-16 18:09:30 -0400

    segundo

---
+++ loginVista.php
@@ -0,0 +1,29 @@
+<?php require_once 'includes/cabecera.php'; ?>
+		
+		
+<!-- CAJA PRINCIPAL -->
+<div class="container login-container">
+            <div class="row">
+                <div class="col-sm-2"></div>
+                <div class="col-md-8 login-form-2">
+                    <h3>Inicio de sesión</h3>
+                    <form>
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Email" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" placeholder="Contraseña" value="" />
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-primary" value="Login" />
+                        </div>
+                        <div class="form-group">
+
+                            <a href="#" class="ForgetPwd" value="Login">Olvidaste tu contraseña</a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+			
+<?php require_once 'includes/pie.php'; ?>
